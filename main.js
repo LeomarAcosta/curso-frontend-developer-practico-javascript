fd2381361c174a7cee119f2e13ed1a6520390d96
Leomar Acosta <leomar4113@gmail.com>
2023-04-08 09:22:41 -0400

    ultima modificaciones

--- main.js
+++ main.js
@@ -1,8 +1,85 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const menuHamIcon = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
+const aside = document.querySelector('.product-detail')
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
+menuHamIcon.addEventListener('click', toggleMobileMenu);
+menuCarritoIcon.addEventListener('click', toggleCarritoAside);
+
+
 
 function toggleDesktopMenu(){
+    const isAsideClosed = aside.classList.contains('inactive');
+    if(!isAsideClosed){
+        aside.classList.add('inactive');
+    }
     desktopMenu.classList.toggle('inactive');
 }
+
+function toggleCarritoAside(){
+    const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
+    if(!isMobileMenuClosed){
+        mobileMenu.classList.add('inactive');
+    }
+    aside.classList.toggle('inactive');
+}
+
+function toggleMobileMenu(){
+    const isAsideClosed = aside.classList.contains('inactive');
+
+    mobileMenu.classList.toggle('inactive');
+
+    if(!isAsideClosed){
+        aside.classList.add('inactive');
+    }
+} 
+
+/*Creo mi Array Para introducir elementos atraves de Html y el DOM */
+const productList=[];
+    productList.push({
+        name: 'Bike',
+        price: 120,
+        image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+
+    });
+    productList.push({
+        name: 'Tv',
+        price: 420,
+        image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+
+    });
+    productList.push({
+        name: 'Pc',
+        price: 520,
+        image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+
+    });
+    productList.push({
+        name: 'Mouse',
+        price: 20,
+        image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+
+    });
+
+    for (product of productList){
+        const productCard = document.createElement('div');
+        productCard.classList.add('product-card');
+
+        const img = document.createElement('img');
+        img.setAttribute('src', product.image);
+
+        const productInfo = document.createElement('div');
+        productInfo.classList.add('product-Info');
+
+        const productInfoDiv = document.createElement('div');
+
+        const productPrice = document.createElement('div');
+        productPrice.innerText = '$' + product.price;
+
+        const productName = document.createElement('div');
+        productName.innerText = '$' + product.price;
+
+    }
